adf9afb2e0920374bf9e6992d17cbf404de6971a
M. Sócrates Soares <mateussocrates@gmail.com>
2016-09-07 19:31:36 -0300

    Add files via upload

---
+++ default.php
@@ -0,0 +1,31 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>LoadingIndex</title>
+<link href="css/ResetAll.css" rel="stylesheet" type="text/css" />
+<link rel="icon" href="img/Icone site.png"/>
+<link rel="stylesheet" href="css/imgLoad.css" type="text/css" />
+<link href="JavaScript/imgLoading.js" type="text/javascript" />
+
+<!--
+<script>
+
+
+	setTimeout("window.location='SiteCompleto/Index.php'", 5000);
+	
+
+</script>
+-->
+</head>
+
+<body oncontextmenu="return false" background="img/Fundo-studio-branco.png" width="100%">
+
+
+<center>
+<img oncontextmenu="return false" src="img/gif-loading-logo.gif" width="200" id="imgLoad" />
+<h2 id="h2">EM BREVE</h2>
+</center>
+
+</body>
+</html>
